7e84910b7a9a772573c906d1609503a709672a9c
Boofa Ubuntu <106235648+RyleyBoofa@users.noreply.github.com>
2025-08-24 16:38:49 +1000

    feat(Book): create Book constructor and add func
    Implemented Book constructor and addBookToLibrary functions along with
    placeholder displayBooks function and Book objects.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,43 @@
-console.log("Hello, odin-library");
+// The Odin Project - Project: Library
+
+const library = [];
+
+function Book(title, author, pages, read, id) {
+    if (!new.target) {
+        throw Error("Book constructor must be called with the 'new' keyword.");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    if (
+        typeof title != "string" ||
+        typeof author != "string" ||
+        !Number.isInteger(pages) ||
+        pages <= 0 ||
+        typeof read != "boolean"
+    ) {
+        throw Error(
+            "Books must be added with: addBookToLibrary(title <string>, author <string>, pages <int>, read <bool>)"
+        );
+    }
+
+    const id = crypto.randomUUID();
+    const book = new Book(title, author, pages, read, id);
+    library.push(book);
+}
+
+function displayBooks() {
+    library.forEach((book) => {
+        console.log(book);
+    });
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 320, false);
+addBookToLibrary("Harry Potter", "J.K. Rowling", 309, true);
+
+displayBooks();
