5a47ec2816017bb2230afe96683eeeec17bdddd0
Boofa Legion Linux <106235648+RyleyBoofa@users.noreply.github.com>
2025-08-25 11:51:18 +1000

    feat(library): add library functionality and gui
    Implemented rudimentary GUI that displays books in a grid and has a
    button and modal dialog to add a new book.
    Added JS to handle creating new book using form data.

--- src/script.js
+++ src/script.js
@@ -1,6 +1,34 @@
 // The Odin Project - Project: Library
 
 const library = [];
+const libraryContainer = document.querySelector(".lib-container");
+const newBookBtn = document.querySelector("#new-book-button");
+const newBookDialog = document.querySelector("#new-book-dialog");
+const confirmBtn = document.querySelector("#confirm");
+const newTitle = document.querySelector("#title");
+const newAuthor = document.querySelector("#author");
+const newPages = document.querySelector("#pages");
+const newRead = document.querySelector("#read");
+
+newBookBtn.addEventListener("click", () => {
+    newBookDialog.showModal();
+});
+
+confirmBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const newBook = addBookToLibrary(
+        newTitle.value,
+        newAuthor.value,
+        Number(newPages.value),
+        newRead.value === "yes" ? true : false
+    );
+    if (newBook === null) {
+        alert("Failed to add new book. Please ensure form is filled out correctly.");
+        return;
+    }
+    displayNewBook(newBook);
+    newBookDialog.close();
+});
 
 function Book(title, author, pages, read, id) {
     if (!new.target) {
@@ -21,23 +49,57 @@ function addBookToLibrary(title, author, pages, read) {
         pages <= 0 ||
         typeof read != "boolean"
     ) {
-        throw Error(
-            "Books must be added with: addBookToLibrary(title <string>, author <string>, pages <int>, read <bool>)"
+        console.error(
+            "ERROR: Books must be added with: addBookToLibrary(title <string>, author <string>, pages <int>, read <bool>)"
         );
+        return null;
     }
 
     const id = crypto.randomUUID();
     const book = new Book(title, author, pages, read, id);
     library.push(book);
+    return book;
 }
 
-function displayBooks() {
+function displayNewBook(book) {
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book");
+
+    const title = document.createElement("h2");
+    title.classList.add("book-title");
+    title.innerHTML = `${book.title}`;
+    bookElement.appendChild(title);
+
+    const author = document.createElement("p");
+    author.classList.add("book-author");
+    author.innerHTML = `${book.author}`;
+    bookElement.appendChild(author);
+
+    const pages = document.createElement("p");
+    pages.classList.add("book-pages");
+    pages.innerHTML = `${book.pages} pages`;
+    bookElement.appendChild(pages);
+
+    const read = document.createElement("p");
+    read.classList.add("book-read");
+    read.innerHTML = book.read ? "Read" : "Not read";
+    bookElement.appendChild(read);
+
+    const id = document.createElement("p");
+    id.classList.add("book-id");
+    id.innerHTML = `ID: ${book.id}`;
+    bookElement.appendChild(id);
+
+    libraryContainer.appendChild(bookElement);
+}
+
+function displayLibrary() {
     library.forEach((book) => {
-        console.log(book);
+        displayNewBook(book);
     });
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 320, false);
 addBookToLibrary("Harry Potter", "J.K. Rowling", 309, true);
 
-displayBooks();
+displayLibrary();
